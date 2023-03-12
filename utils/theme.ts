e71db16ee9633fa90222af11a24eab16db23fffb
Jayden <yawzyn001@mymail.sim.edu.sg>
2023-03-12 13:14:08 +0800

    change theme

--- utils/theme.ts
+++ utils/theme.ts
@@ -1,8 +1,7 @@
-import { ColorMode, extendTheme, type ThemeConfig } from '@chakra-ui/react'
+import { extendTheme, type ThemeConfig } from '@chakra-ui/react'
 
 const config: ThemeConfig = {
-  initialColorMode:
-    (localStorage.getItem('chakra-ui-color-mode') as ColorMode) || 'dark',
+  initialColorMode: 'dark',
   useSystemColorMode: true,
 }
 
